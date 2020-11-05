afea80891b85ed40a98c2e5920a9fbf93a49e214
RemyRobert <remy.robert@epsi.fr>
2020-11-05 11:59:31 +0100

    undo web.php

--- routes/web.php
+++ routes/web.php
@@ -52,6 +52,3 @@ Route::get('/logout','LoginController@logout');
 Route::get('/createPost','PostsController@index');
 
 Route::post('/createAPost','PostsController@createAPost');
-
-
-Route::post("/updateProfil", [UsersController::class, 'updateProfil']);
